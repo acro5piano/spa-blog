9b4f863f067a75247eafff9f05cbf96774311396
gosho-kazuya <gosho-kazuya@919.jp>
2017-04-02 16:39:38 +0900

    change deploy command

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -17,7 +17,35 @@ echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
 echo $CIRCLE_SHA1
 
 eb use $EB_APP_NAME
-cp .env.production .env
+cat <<EOF > .env
+APP_ENV=production
+APP_KEY=
+APP_DEBUG=false
+APP_LOG_LEVEL=debug
+APP_URL=http://localhost
+
+BROADCAST_DRIVER=log
+CACHE_DRIVER=file
+SESSION_DRIVER=file
+QUEUE_DRIVER=sync
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_DRIVER=smtp
+MAIL_HOST=mailtrap.io
+MAIL_PORT=2525
+MAIL_USERNAME=null
+MAIL_PASSWORD=null
+MAIL_ENCRYPTION=null
+
+PUSHER_APP_ID=
+PUSHER_APP_KEY=
+PUSHER_APP_SECRET=
+
+DB_CONNECTION=sqlite
+EOF
 
 npm run production
 eb deploy
