3c54f707d6e24a0e240a86a9d272b31f388071e2
gosho-kazuya <gosho-kazuya@919.jp>
2017-04-09 20:12:47 +0900

    add copy

--- resources/assets/js/components/Top.vue
+++ resources/assets/js/components/Top.vue
@@ -5,7 +5,7 @@
     <div v-else>
       <div class="top-wrapper">
         <div class="container">
-          <h1>LEARN TO CODE.<br>LEARN TO BE CREATIVE.</h1>
+          <h1>猫に小判。豚に真珠。<br>あなたにコード。</h1>
           <p>codecheerはプログラミングを学習する人々を繋ぐコミュニケーションサイトです。<br>登録ユーザーと応援し合い、学習ペースを共有する中で自分の技術力を高めよう！</p>
           <div class="btn-wrapper">
             <a href="#" class="btn signup">新規登録はこちら</a>
