15042c2c341cdff52e2fa7b5b624d0b989688603
gosho-kazuya <gosho-kazuya@919.jp>
2017-04-19 23:30:29 +0900

    add login link to top navbar

--- resources/assets/js/components/Layouts/Navbar.vue
+++ resources/assets/js/components/Layouts/Navbar.vue
@@ -1,20 +1,41 @@
 <template>
   <el-row class="navbar">
     <el-menu theme="dark" :default-active="'/'" :router="true" mode="horizontal" @select="handleSelect">
-      <el-col :span="20">
+
+      <el-col :span="14">
         <el-menu-item index="/">Codecheer</el-menu-item>
         <el-menu-item index="/home" v-if="userState.authenticated">Home</el-menu-item>
         <el-menu-item index="/articles/new" v-if="userState.authenticated">New item</el-menu-item>
       </el-col>
-      <el-col :span="4" v-if="userState.authenticated">
+
+      <el-col :span="4" :push="6" v-if="userState.authenticated">
         <el-submenu index="#">
           <template index="#" slot="title">{{ userState.user.name }}</template>
           <el-menu-item index="#" @click="logout">Log out</el-menu-item>
         </el-submenu>
       </el-col>
-      <el-col :span="4" v-else>
-        <el-menu-item index="/login">Log in</el-menu-item>
+
+      <el-col :span="10" v-if="! userState.authenticated">
+        <el-menu-item index="#">
+          <el-input id="email" type="text" v-model="email" @keyup.enter.native="login" size="small" required autofocus>
+          </el-input>
+        </el-menu-item>
+
+        <el-menu-item index="#">
+          <el-input id="password" type="password" v-model="password" @keyup.enter.native="login" size="small" required autofocus>
+          </el-input>
+        </el-menu-item>
+
+        <el-menu-item index="#">
+          <!-- <el&#45;button type="primary" @click="login">Login</el&#45;button> -->
+          <el-button type="primary" size="small" @click="login">Login</el-button>
+        </el-menu-item>
       </el-col>
+
+        <!-- <div class="form&#45;group"> -->
+        <!--   <input type="checkbox" name="remember"> Remember Me -->
+        <!-- </div> -->
+
     </el-menu>
   </el-row>
 </template>
@@ -25,13 +46,23 @@
   export default {
     data (){
       return {
-        userState: userStore.state
+        userState: userStore.state,
+        email: '',
+        password: ''
       }
     },
     methods: {
       logout() {
         userStore.logout( () => {
           this.$router.push('/')
+        })
+      },
+      login () {
+        userStore.login(this.email, this.password, res => {
+          this.$router.push('/')
+        }, error => {
+          this.show_alert = true
+          this.alert_message = 'Wrong email or password.'
         })
       },
       handleSelect() {
